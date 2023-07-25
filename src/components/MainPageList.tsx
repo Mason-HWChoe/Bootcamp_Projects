6abf47c975c53b438408afe8d48da8879cda9fd9
Project <hwchoe86@gmail.com>
2023-07-25 14:14:10 +0900

    random data code added

--- src/components/MainPageList.tsx
+++ src/components/MainPageList.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useFetch } from '../hooks/useFetch';
+import { Item, useFetch } from '../hooks/useFetch';
 import styles from './MainPageList.module.css';
 
 export default function MainPageList() {
   const CAMPING_API_KEY = process.env.REACT_APP_CAMPING_API_KEY;
-  const url = `https://apis.data.go.kr/B551011/GoCamping/basedList?serviceKey=${CAMPING_API_KEY}&numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json`;
+  const url = `https://apis.data.go.kr/B551011/GoCamping/basedList?serviceKey=${CAMPING_API_KEY}&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json`;
 
   const { data, isLoading } = useFetch(url);
+
+  const [randomData, setRandomData] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const getRandomNumber = (count: number, total: number): number[] => {
+      const randomNumbers = new Set<number>();
+
+      while (randomNumbers.size < count) {
+        const randomNumber = Math.floor(Math.random() * total);
+        randomNumbers.add(randomNumber);
+      }
+      return Array.from(randomNumbers);
+    };
+
+    if (data.length > 0) {
+      const randomCount = 12;
+      const randomIndexs = getRandomNumber(randomCount, data.length);
+      console.log(randomIndexs);
+      const randomItems = randomIndexs.map((index) => data[index]);
+      setRandomData(randomItems);
+    }
+  }, [data]);
 
   if (isLoading) {
     return (
@@ -40,7 +63,7 @@ export default function MainPageList() {
 
       <div className="container mt-5">
         <div className="row">
-          {data.map((item) => (
+          {randomData.map((item) => (
             <div className="col-md-3" key={item.contentId}>
               <div className="card mb-5">
                 <div className={styles.imgWrapper}>
